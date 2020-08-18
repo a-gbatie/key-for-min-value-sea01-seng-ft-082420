9aaf69f520b034b9cce799e3324fd172ef808e82
Ange Batie <ange.batie@gmail.com>
2020-08-18 01:13:11 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,6 +6,7 @@ def key_for_min_value(name_hash)
   lowest_value = nil
   name_hash.each do |key, value|
     if lowest_value == nill || value < lowest_value
-      
+      lowest_key = key
+      lowest_value = value
 
 end
